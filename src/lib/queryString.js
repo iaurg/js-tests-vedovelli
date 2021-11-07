1e8ecf6dbc848b38ef6f65558466838589214264
Italo aurelio <italoaurelior@gmail.com>
2021-11-07 17:50:53 -0300

    refactor function

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,5 +1,9 @@
 const queryString = obj =>
   Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
+    .map(([key, value]) => {
+      if (typeof value == 'object' && !Array.isArray(value))
+        throw new Error('Invalid params');
+      return `${key}=${value}`;
+    })
     .join('&');
 module.exports.queryString = queryString;
