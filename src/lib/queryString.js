f57b248ffb5e1baafee9381ef491408cece769ad
Italo aurelio <italoaurelior@gmail.com>
2021-11-07 17:56:53 -0300

    refactor ket value to string

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,9 +1,8 @@
-const queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => {
-      if (typeof value == 'object' && !Array.isArray(value))
-        throw new Error('Invalid params');
-      return `${key}=${value}`;
-    })
-    .join('&');
+const keyValueToString = ([key, value]) => {
+  if (typeof value == 'object' && !Array.isArray(value))
+    throw new Error('Invalid params');
+  return `${key}=${value}`;
+};
+
+const queryString = obj => Object.entries(obj).map(keyValueToString).join('&');
 module.exports.queryString = queryString;
