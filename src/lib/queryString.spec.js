cb23f866dfe2779f88a32f0d86b399106605e87e
Italo aurelio <italoaurelior@gmail.com>
2021-11-07 17:51:00 -0300

    add more tests

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,4 +9,27 @@ describe('object to queryString', () => {
 
     expect(queryString(obj)).toBe('name=italo&profession=developer');
   });
+
+  it('should create a valid query string when an array is passed', () => {
+    const obj = {
+      name: 'iaurg',
+      skills: ['JS', 'CSS'],
+    };
+
+    expect(queryString(obj)).toBe('name=iaurg&skills=JS,CSS');
+  });
+
+  it('should throw an error with deep nested objects', () => {
+    const obj = {
+      name: 'italo',
+      skills: {
+        web: 'JS',
+        life: 'drive',
+      },
+    };
+    console.log(queryString(obj));
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
